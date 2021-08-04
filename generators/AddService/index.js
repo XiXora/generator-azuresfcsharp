4e53999b4decc519843d67ef6d761c86d1c424d5
Damon Stephenson <me@xixora.net>
2021-08-04 19:52:54 +0000

    Convert AddService to class

--- generators/AddService/index.js
+++ generators/AddService/index.js
@@ -1,63 +1,65 @@
-'use strict';
+"use strict";
 
-var path      = require('path')
-, generators    = require('yeoman-generator')
-, yosay     = require('yosay')
+var Generator = require("yeoman-generator");
 
-var JavaGenerator = generators.Base.extend({
-    constructor: function () {
-        generators.Base.apply(this, arguments);
+var ClassGenerator = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
 
-        this.desc('Generate Service Fabric CSharp app template');
-        var chalk = require('chalk');
-        if (this.config.get('projName')) {
-            console.log(chalk.green("Setting project name to", this.config.get('projName')));
-        } else {
-            var err = chalk.red("Project name not found in .yo-rc.json. Exiting ..."); 
-            throw err;
-        }
-    },
-
-    prompting: function () {
-        var done = this.async();
-
-        var prompts = [{
-            type: 'list'
-            , name: 'frameworkType'
-            , message: 'Choose a framework for your service'
-            , default: this.config.get('frameworkType')
-            , choices: ["Reliable Actor Service", "Reliable Stateless Service","Reliable Stateful Service"]
-            }];
-
-            this.prompt(prompts, function (props) {
-            this.props = props;
-            this.config.set(props);
-
-            done();
-        }.bind(this));
-    },
-
-    writing: function() {
-        var libPath = "REPLACE_SFLIBSPATH";
-        var isAddNewService = true; 
-        if (this.props.frameworkType == "Reliable Actor Service") {
-            this.composeWith('azuresfcsharp:CoreCLRStatefulActor', {
-            options: { libPath: libPath, isAddNewService: isAddNewService }
-        });
-        } else if (this.props.frameworkType == "Reliable Stateless Service") {
-            this.composeWith('azuresfcsharp:CoreCLRStatelessService', {
-            options: { libPath: libPath, isAddNewService: isAddNewService }
-            });
-        } else if (this.props.frameworkType == "Reliable Stateful Service"){
-        this.composeWith('azuresfcsharp:CoreCLRStatefulService', {
-            options: { libPath: libPath, isAddNewService: isAddNewService }
-           });
-        }
-    },
-    end: function () {
-        this.config.save();
+    this.desc("Generate Service Fabric CSharp app template");
+    var chalk = require("chalk");
+    if (this.config.get("projName")) {
+      console.log(
+        chalk.green("Setting project name to", this.config.get("projName"))
+      );
+    } else {
+      var err = chalk.red("Project name not found in .yo-rc.json. Exiting ...");
+      throw err;
     }
-});
+  }
 
-module.exports = JavaGenerator;
+  async prompting() {
+    var prompts = [
+      {
+        type: "list",
+        name: "frameworkType",
+        message: "Choose a framework for your service",
+        default: this.config.get("frameworkType"),
+        choices: [
+          "Reliable Actor Service",
+          "Reliable Stateless Service",
+          "Reliable Stateful Service",
+        ],
+      },
+    ];
 
+    await this.prompt(prompts).then(function (props) {
+      this.props = props;
+      this.config.set(props);
+    });
+  }
+
+  writing() {
+    var libPath = "REPLACE_SFLIBSPATH";
+    var isAddNewService = true;
+    if (this.props.frameworkType == "Reliable Actor Service") {
+      this.composeWith("azuresfcsharp:CoreCLRStatefulActor", {
+        options: { libPath: libPath, isAddNewService: isAddNewService },
+      });
+    } else if (this.props.frameworkType == "Reliable Stateless Service") {
+      this.composeWith("azuresfcsharp:CoreCLRStatelessService", {
+        options: { libPath: libPath, isAddNewService: isAddNewService },
+      });
+    } else if (this.props.frameworkType == "Reliable Stateful Service") {
+      this.composeWith("azuresfcsharp:CoreCLRStatefulService", {
+        options: { libPath: libPath, isAddNewService: isAddNewService },
+      });
+    }
+  }
+
+  end() {
+    this.config.save();
+  }
+};
+
+module.exports = ClassGenerator;
